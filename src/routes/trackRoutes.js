4e13fff3dd8f636dc0412d6c909ac0177f041f59
ErickInformatica <36520780+ErickInformatica@users.noreply.github.com>
2019-07-24 13:20:06 -0600

    Fix routes Track

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -8,9 +8,9 @@ var md_auth = require('../middlewares/autheticated');
 
 //Rutas
 var api = express.Router();
-api.get('/tracks', md_auth.ensureAuth, TrackController.gettracks);
-api.post('/track', md_auth.ensureAuth, TrackController.addtracks);
-api.get('/track/:id', md_auth.ensureAuth, TrackController.gettrack)
+api.get('/tracks',  TrackController.gettracks);
+api.post('/track', TrackController.addtracks);
+api.get('/track/:id',  TrackController.gettrack)
 
 
 
